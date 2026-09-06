5b9c8eaac0aee0d6f39da5f5498821981c23931d
Guilherme Augusto Goettnauer <guilhermegoet@gmail.com>
2026-09-05 22:08:00 -0300

    Adiciona validações ao SendEmailRequest

--- src/main/java/com/goett/emailsender/dto/SendEmailRequest.java
+++ src/main/java/com/goett/emailsender/dto/SendEmailRequest.java
@@ -1,7 +1,16 @@
 package com.goett.emailsender.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public record SendEmailRequest(
+        @NotBlank(message = "Email não pode estar vazio")
+        @Email(message = "Email inválido")
         String to,
+        
+        @NotBlank(message = "Assunto não pode estar vazio")
         String subject,
-        String cargo) {
-}
+        
+        @NotBlank(message = "Cargo não pode estar vazio")
+        String cargo
+) {}
